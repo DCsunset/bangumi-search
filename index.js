e85bc3ac9a868905c58ef41fc65c439da9ca36d7
DCsunset <dcsunset@protonmail.com>
2020-01-26 02:44:20 -0800

    feat: use prettyjson to beautify output

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 const bangumiSearch = require('./bangumiSearch');
+const prettyjson = require('prettyjson');
 
 if (process.argv.length < 3) {
 	console.log("No keywords specified");
@@ -9,5 +10,5 @@ if (process.argv.length < 3) {
 
 const results = bangumiSearch(process.argv[2]);
 for (const r of results) {
-	console.log(r);
+	console.log(prettyjson.render(r), '\n');
 }

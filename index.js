7487583048dce2c013c848f51679fc0bbe891a45
DCsunset <dcsunset@protonmail.com>
2020-01-26 19:32:57 -0800

    feat: add more options for cli

--- index.js
+++ index.js
@@ -2,13 +2,34 @@
 
 const bangumiSearch = require('./bangumiSearch');
 const prettyjson = require('prettyjson');
+const _ = require('lodash');
 
-if (process.argv.length < 3) {
-	console.log("No keywords specified");
-	process.exit();
-}
+const argv = require('yargs')
+	.usage('Usage: $0 <str> [options]')
+	.help('h')
+	.demandCommand(1)
+	.default({
+		number: 5
+	})
+	.boolean('title')
+	.describe('title', 'Show titles only in results')
+	.number('number')
+	.describe('number', 'Limit the number of results. -1 for unlimited.')
+	.alias('t', 'title')
+	.alias('n', 'number')
+	.alias('h', 'help')
+	.alias('v', 'version')
+	.argv;
 
-const results = bangumiSearch(process.argv[2]);
-for (const r of results) {
-	console.log(prettyjson.render(r), '\n');
-}
+const results = bangumiSearch(argv._[0]);
+let count = 0;
+results.forEach((result, index) => {
+	if (argv.number > 0 && index >= argv.number)
+		return;
+	++count;
+	if (argv.title)
+		result = _.pick(result, ['title', 'titleTranslate']);
+	console.log(`--- Result ${index + 1} ---\n${prettyjson.render(result)}\n`);
+})
+
+console.log(`${count} results showed. ${results.length} in total.`);
